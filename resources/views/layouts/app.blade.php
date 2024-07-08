39b6fd54dd8b278231182bac79cb41a77b75bd2f
sinzin075 <sinzin2001075@gmail.com>
2024-07-08 21:34:06 +0900

    breeze install and Changed the greeting column in the user table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,11 @@
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                    <ul>
+                        <li><a href="">Blog</a></li><!--blog画面へ遷移-->
+                        <li><a href="">Event</a></li><!--event画面へ遷移-->
+                        <li><a href="">Users</a></li><!--userstatus画面へ遷移-->
+                    </ul>
                     </div>
                 </header>
             @endif
